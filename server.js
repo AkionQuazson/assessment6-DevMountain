707d61a030424e2a5f1fe990ffafc34ffc774845
spencer <spencer@homelightarchive.com>
2022-11-05 14:20:58 -0600

    started setup

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const {shuffleArray} = require('./utils')
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
 app.get('/api/robots', (req, res) => {
     try {
         res.status(200).send(botsArr)
