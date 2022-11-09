54bfdb6642246f870c98144048e3ecfa608f124b
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2022-11-08 18:32:00 -0700

    Added automated tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -19,10 +19,23 @@ test('Title shows up when page loads', async () => {
     expect(displayed).toBe(true)
 })
 
-// test('Draw button displays choices', async () => {
-//     expect()
-// })
+test('Draw button displays choices', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    driver.executeScript("arguments[0].click();", draw);
 
-// test('', async () => {
-//     expect()
-// })
+    const displayed = await driver.findElement(By.id('results')).isDisplayed();
+
+    expect(displayed).toBe(true);
+})
+
+test('Add to duo ', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    driver.executeScript("arguments[0].click();", draw);
+
+    const add = await driver.findElement(By.className('bot-btn'))
+    driver.executeScript("arguments[0].click();", add);
+
+    const selected = await driver.findElement(By.linkText('Remove from Duo'));
+
+    expect().toBe(!null);
+})
